61aee64e5943c72c3153407dd6b6195a98593677
akabishau <aleksey.kabishau@icloud.com>
2022-12-30 17:16:34 -0500

    handle message form submit click

--- js/index.js
+++ js/index.js
@@ -16,3 +16,18 @@ for (skill of mySkills) {
     skillBullet.textContent = skill;
     skillsList.appendChild(skillBullet);
 }
+
+
+// handle message form submit
+const messageForm = document.getElementsByName('leave_message')[0];
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // in this case prevents refreshing page after submit clicked
+    console.log(event.target);
+
+    const name = event.target.name;
+    const email = event.target.email;
+    const message = event.target.message;
+
+    console.log(`${name.value} + ${email.value} + ${message.value}`);
+    event.target.reset();
+});
